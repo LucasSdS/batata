cc7d958dcac2a1d201eaaf7d55c8b0c80b68ce29
Pedro Henrique <pedrohcnunes@hotmail.com>
2015-08-04 23:53:15 -0300

    Adição da aba contato

--- resources/views/contato.blade.php
+++ resources/views/contato.blade.php
@@ -1,64 +1,39 @@
 <!doctype html>
 <html lang="pt-BR">
 <head>
-    <meta charset="utf-8">
-    <title>AG¡T EM …LE</title>
+    <meta charset="UTF-8">
+    <title>Bom Lugar Viagens - Contato</title>
 </head>
 <body>
-<!-- Isto È um coment·rio, nada disso
-ir· aparecer no site. -->
 <header>
     <nav>
-        <p><sub>Este È um</sub> menu <sup>simples.</sup></p>
         <ul>
-            <li><a href="<?php echo url('/'); ?>">HOME</a></li>
-            <li><a href="<?php echo url('admin'); ?>">AREA ADM</a></li>
-            <li><a href="<?php echo url('contato'); ?>"><img src = "images/fone.png" title = "Contato" >CONTATO</a></li>
+            <li><a href="<?php echo url('/'); ?>">P¡GINA INICIAL</a></li>
+            <li><a href="<?php echo url('viagens'); ?>">VIAGENS</a></li>
+            <li><a href="<?php echo url('quemsomos'); ?>">QUEM SOMOS</a></li>
+            <li><a href="<?php echo url('contato'); ?>">CONTATO</a></li>
+            <li><a href="<?php echo url('admin'); ?>">ADMIN</a></li>
         </ul>
-        <ol>
-            <li><strong>HO</strong>ME</li>
-            <li>AREA ADM</li>
-            <li>CONTATO</li>
-        </ol>
     </nav>
 </header>
 <hr>
-<br><br>
+<br>
 
 <section>
-    <p> Legal, me envie seus dados que vou te responder.</p>
-    <form action="" method="get">
-        <label for="login">Username</label>
-        <input type="text" name="login" id="login">
+    <form action = "" method = "get" name="1">
 
-        <label for="senha" >Senha</label>
-        <input type="password" name="senha" id="senha">
-        <br><br>
-
-        <label for="sexoM">Sexo</label>
-        <input type="radio" name="sexo" id="sexoM"> M <br>
-        <input type="radio" name="sexo" id="sexoF"> F
-
-
-        <select name="estado" id="estado">
-            <option value="...">...</option>
-            <option value="RJ">Rio de Janeiro</option>
-            <option value="MG">Minas Gerais</option>
-            <option value="SP">S„o Paulo</option>
-        </select>
-
-        <textarea name="descricao" id="descricao" cols="30" rows="10"></textarea>
-
-        <input type="submit" value="Enviar">
-        <input type="reset" value="Limpar">
-
-        <form action="http://google.com/search" method="get" target="_blank">
-            <input type="text" name="q">
-            <input type="submit" value="Search">
-        </form>
-
-
+        <label for = "nome">Nome</label>
+        <input type = "text" name = "nome" id = "nome"><br>
+        <label for = "email">Email</label>
+        <input type = "email" name = "email" id = "email"><br>
+        <label for = "text">Mensagem</label><br>
+        <textarea rows = "10" cols = "50"></textarea><br>
+        <input type = "submit" value = "Enviar">
+        <input type = "reset" value = "Limpar">
     </form>
+        <br>
+        <br>
 </section>
+
 </body>
 </html>
